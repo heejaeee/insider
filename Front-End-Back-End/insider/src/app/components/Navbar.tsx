787b7a5855e1b8d686effb9ea69ba6adf61baba1
Orlando Scheil <orlandos.ks05@gmail.com>
2024-04-08 18:56:28 -0700

    itty bitty change

--- Front-End-Back-End/insider/src/app/components/Navbar.tsx
+++ Front-End-Back-End/insider/src/app/components/Navbar.tsx
@@ -1,13 +1,12 @@
 import { navLinks } from "../constants";
 
-
 const Navbar = () => {
     return (
         <nav className="w-full flex py-6 justify-between items-center navbar mx-auto">
-            <ul className="list-none sm:flex hidden justify-center items-center flex-1 gap-x-[3vw]" >
+            <ul className="list-none sm:flex hidden justify-center items-center flex-1 gap-x-[3vw]">
                 {navLinks.map((nav, index) => (
                     <li key={nav.id} className="">
-                        <a href={`#${nav.id}`}>
+                        <a href={`#${nav.id}`} className="text-gray-600 hover:text-gray-900">
                             {nav.title}
                         </a>
                     </li>
