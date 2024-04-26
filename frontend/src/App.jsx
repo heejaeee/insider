e3e01d0d1191f3eb5594fef404e121cd6b395823
Orlando Scheil <orlandos.ks05@gmail.com>
2024-04-26 14:12:47 -0700

    seperated frontend implemented navbar, card system, sorting and search for people.

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from './components/Navbar/Navbar';
+import PeoplePage from './pages/PeoplePage';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <PeoplePage />
+    </div>
+  );
+}
+
+export default App;
