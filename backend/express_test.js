b889fa7eddb68d9a5466cb4a05738bff09de9829
HeeJae Kwon <hkwon4@uw.edu>
2024-05-03 13:55:33 -0700

    test update

--- backend/express_test.js
+++ backend/express_test.js
@@ -7,6 +7,14 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
+app.get("/contacts", (req, res) => {
+    res.status(200).send("Contact Page");
+});
+
+app.post("/contacts", (req, res) => {
+    res.status(201).send("Create Contacts");
+});
+
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
 });
