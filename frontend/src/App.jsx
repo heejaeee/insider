56086a7a3bff9551481075096989665ff909e6f8
Orlando Scheil <orlandos.ks05@gmail.com>
2024-05-08 12:26:53 -0700

    implemented basic routing system for mpa fronrtend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,25 @@
 import Navbar from './components/Navbar/Navbar';
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
 import PeoplePage from './pages/PeoplePage';
+
+// router gonna be in here, i should consider changing the names of the pages so that they make more sense bc "PeoplePage" is not that satisfying
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <PeoplePage />
+  },
+])
+
 
 function App() {
   return (
-    <div>
+    <>
       <Navbar />
-      <PeoplePage />
-    </div>
+      <RouterProvider router={router} />
+    </>
   );
 }
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+// using router in App.jsx to navigate between pages rather than main.jsx
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
